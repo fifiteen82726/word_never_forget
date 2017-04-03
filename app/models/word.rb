5b86e47ff4404f762b3db1908f40a0b262e24a4e
fifiteen82726 <fifiteen82726@gmail.com>
2017-04-03 20:10:31 +0800

    Give word, return description

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Word < ApplicationRecord
+  BASE_URL = 'http://www.iciba.com/'
+
+  def check(word)
+    word_url = BASE_URL + word.to_s
+    page = Nokogiri::HTML(open(word_url))
+    word = page.css('h1').first.children.text.delete(" \t\r\n")
+    return false if word.blank?
+    description = ''
+    page.css('.base-list.switch_part li').each do |list|
+      attributr = list.css('.prop')[0].children.text
+      description.concat(attributr + ' ')
+      description.concat(list.css('p')[0].children.text.delete(" \t\r\n"))
+      description.concat('\n ')
+    end
+
+    return {
+      :word => word,
+      :description => description
+    }
+  end
 end
